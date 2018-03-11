305affec829a0074fdcaeb64545b9cbea7dd9035
Christian Bäuerlein <fabrik42@gmail.com>
2018-03-11 14:39:41 +0100

    Bootstrapping CoreUI

---
+++ src/containers/Full/Full.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import {Link, Switch, Route, Redirect} from 'react-router-dom';
+import {Container} from 'reactstrap';
+import Header from '../../components/Header/';
+import Sidebar from '../../components/Sidebar/';
+import Aside from '../../components/Aside/';
+import Footer from '../../components/Footer/';
+
+import Dashboard from '../../views/Dashboard/';
+
+class Full extends Component {
+  render() {
+    return (
+      <div className="app">
+        <Header />
+        <div className="app-body">
+          <main className="main">
+            <Container fluid>
+              <Dashboard/>
+            </Container>
+          </main>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Full;
