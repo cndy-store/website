bd91600f59c54ce5b1a6217133ad63b95714ed5a
Christian Bäuerlein <fabrik42@gmail.com>
2018-03-12 11:09:50 +0100

    Be honest, I don’t drink that much coffee

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ class Footer extends Component {
         <span>
           <a href="https://cndy.store">CNDY</a> &copy; 2018
         </span>
-        <span className="ml-auto">Made with ❤, ☕ and 🍭</span>
+        <span className="ml-auto">Made with ❤ and 🍭</span>
       </footer>
     );
   }
