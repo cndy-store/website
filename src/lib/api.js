0e0cbd07dbd7a07f10cb22a1a409cfbd41cc2a05
Christian Bäuerlein <fabrik42@gmail.com>
2018-07-01 17:07:40 +0200

    Add API support for time windows

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,14 +2,16 @@ import $ from 'jquery';
 
 const baseURL = 'https://api.cndy.store';
 
-const url = path => `${baseURL}/${path}`;
+const buildUrl = (path, params) => `${baseURL}/${path}?${$.param(params)}`;
 
-const loadLatest = () => {
-  return $.getJSON(url('stats/latest'));
+const loadLatest = (params = {}) => {
+  const url = buildUrl('stats/latest', params);
+  return $.getJSON(url);
 };
 
-const loadStats = () => {
-  return $.getJSON(url('stats'));
+const loadStats = (params = {}) => {
+  const url = buildUrl('stats', params);
+  return $.getJSON(url);
 };
 
 export { loadStats, loadLatest };
